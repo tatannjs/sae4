b4033d49908cfeda10488c4d5ad6f753f191f9c3
Hugo123456712 <64363683+Hugo123456712@users.noreply.github.com>
2025-03-18 13:45:12 +0100

    ajout fenetre modale

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\ProducteurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(ProducteurRepository $producteurRepository): Response
     {
+        // Récupérer tous les producteurs
+        $producteurs = $producteurRepository->findAll();
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'producteurs' => $producteurs,
         ]);
     }
+
 }
